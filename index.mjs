4a09225dd9dc4dc9e8ed56a44c17c936b1d67896
(none) <john@iamjohnhenry.com>
2021-10-13 19:32:19 -0700

    add content disposition header

--- index.mjs
+++ index.mjs
@@ -2,7 +2,10 @@ const randomBytes = (N) => globalThis.crypto.getRandomValues(new Uint8ClampedArr
 const handleRequest = async (request)=> {
   const bytesLength = Number(new URL(request.url).pathname.split('/')[1]) || 1;
   return new Response(randomBytes(bytesLength), {
-    headers:{"Content-Type": "application/octet-stream"}});
+    headers:{
+      "Content-Type": "application/octet-stream",
+      "Content-Disposition": 'attachment; filename="random.bin"'
+  }});
 };
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
