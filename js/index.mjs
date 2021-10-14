c873dec4a51e7e2c671e85bf1cdb876d82e3ba2f
(none) <john@iamjohnhenry.com>
2021-10-14 04:17:11 -0700

    use x-source header

--- js/index.mjs
+++ js/index.mjs
@@ -1,13 +1,12 @@
 const randomBytes = (N) => globalThis.crypto.getRandomValues(new Uint8ClampedArray(N));
 const handleRequest = async (request)=> {
   const url = new URL(request.url);
-  console.log(url)
   const bytesLength = Number(url.pathname.split('/')[1]) || 1;
   return new Response(randomBytes(bytesLength), {
     headers:{
       "Content-Type": "application/octet-stream",
       "Content-Disposition": `attachment; filename="${bytesLength}.bin"`,
-      server:url.host
+      ["x-source"]:url.host
   }});
 };
 addEventListener('fetch', event => {
