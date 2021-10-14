bb909df7f17290246d7acb68309b9c1b59a172ac
(none) <john@iamjohnhenry.com>
2021-10-14 04:13:24 -0700

    echo server

--- js/index.mjs
+++ js/index.mjs
@@ -1,11 +1,14 @@
 const randomBytes = (N) => globalThis.crypto.getRandomValues(new Uint8ClampedArray(N));
 const handleRequest = async (request)=> {
-  const bytesLength = Number(new URL(request.url).pathname.split('/')[1]) || 1;
-  return new Response(randomBytes(bytesLength)/*, {
+  const url = new URL(request.url);
+  console.log(url)
+  const bytesLength = Number(url.pathname.split('/')[1]) || 1;
+  return new Response(randomBytes(bytesLength), {
     headers:{
       "Content-Type": "application/octet-stream",
-      "Content-Disposition": `attachment; filename="${bytesLength}.bin"`
-  }}*/);
+      "Content-Disposition": `attachment; filename="${bytesLength}.bin"`,
+      server:url.host
+  }});
 };
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
