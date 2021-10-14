b537ad4f8cf1f186fd88aa857fbee5f486a6b2e7
(none) <john@iamjohnhenry.com>
2021-10-14 04:01:33 -0700

    'remove headers'

--- js/index.mjs
+++ js/index.mjs
@@ -1,11 +1,11 @@
 const randomBytes = (N) => globalThis.crypto.getRandomValues(new Uint8ClampedArray(N));
 const handleRequest = async (request)=> {
   const bytesLength = Number(new URL(request.url).pathname.split('/')[1]) || 1;
-  return new Response(randomBytes(bytesLength), {
+  return new Response(randomBytes(bytesLength)/*, {
     headers:{
       "Content-Type": "application/octet-stream",
       "Content-Disposition": `attachment; filename="${bytesLength}.bin"`
-  }});
+  }}*/);
 };
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
